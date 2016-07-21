6818c11ee3bdd527730de33e5e8cf113a685cf7f
comnhom92@gmail.com <Chuong123>
2016-07-21 17:33:25 +0700

    init

---
+++ AptechWatch/AptechWatch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AptechWatch.MvcApplication" Language="C#" %>
---
+++ AptechWatch/AptechWatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AptechWatch.Startup))]
+namespace AptechWatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
