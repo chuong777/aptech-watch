9662231eb9bce34b86ee2cc43305c6374c8edda7
phamchitrung <zamahazu000@gmail.com>
2016-07-24 23:14:59 +0700

    Add admin and customer

---
+++ AptechWatch/AptechWatch/Entity/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AptechWatch.Entity
+{
+    public class Admin : BaseEntity
+    {
+        // Role = 0 is super, 1 is normal
+        public int Role { get; set; }
+        [Required]
+        public string RealName { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int PhotoType { get; set; }
+        public virtual ICollection<Watch> News { get; set; }
+        public virtual ICollection<Watch> Chats { get; set; }
+    }
+}
---
+++ AptechWatch/AptechWatch/Entity/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AptechWatch.Entity
+{
+    public class Customer:BaseEntity
+    {
+        [Required]
+        public string Realname { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public virtual ICollection<Watch> Views { get; set; }
+        public virtual ICollection<Watch> Wishes { get; set; }
+        public virtual ICollection<Watch> Carts { get; set; }
+        public virtual ICollection<Watch> Chats { get; set; }
+    }
+}
